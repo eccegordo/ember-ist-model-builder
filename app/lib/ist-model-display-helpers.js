b9910df30c9d1e3fc6082700942bb6a9e15e25df
Tyler Roberts <tyler.roberts@emc.com>
2015-05-04 18:30:38 -0700

    fixed exports again

--- app/lib/ist-model-display-helpers.js
+++ app/lib/ist-model-display-helpers.js
@@ -1,5 +1,5 @@
 import {roundNumber, prettyNumber, prettyPercent, attrToTitle} from 'ember-ist-model-builder/display-helpers';
 import IstModelDisplayHelpers from 'ember-ist-model-builder/display-helpers';
 
-import {roundNumber, prettyNumber, prettyPercent, attrToTitle};
+export {roundNumber, prettyNumber, prettyPercent, attrToTitle};
 export default IstModelDisplayHelpers;
