fc9ae1541262468c03d81142b4fda7ba55a8faff
Tyler Roberts <tyler.roberts@emc.com>
2015-05-26 16:38:46 -0700

    added importable deepSaveArray method

--- app/lib/ist-model-children-helpers.js
+++ app/lib/ist-model-children-helpers.js
@@ -1,2 +1,5 @@
 import IstModelChildrenHelpers from 'ember-ist-model-builder/children-helpers';
+import {deepSaveArray} from 'ember-ist-model-builder/children-helpers';
+
 export default IstModelChildrenHelpers;
+export { deepSaveArray };
